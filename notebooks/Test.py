ebbde8aafd29f240936a0dc9da3a6c9e3344c469
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-01-22 10:39:28 +0100

    Api 1

--- notebooks/Test.py
+++ notebooks/Test.py
@@ -1,1 +1,16 @@
-print ("funktionirt")
+import requests
+
+def get_github_user(username):
+    api_url = f'https://api.github.com/users/{username}'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print(f"Username: {user_data['login']}")
+        print(f"Name: {user_data['name']}")
+        print(f"Public Repositories: {user_data['public_repos']}")
+    else:
+        print(f"Error: {response.status_code}")
+
+# Beispielaufruf
+get_github_user('UngemachM')
