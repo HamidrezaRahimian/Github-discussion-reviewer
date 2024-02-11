1e42358cff9cb4553d205da4400a84d667617e2a
Moritz Ungemach <Moritz.Ungemach@flyeralarm.com>
2024-02-11 21:33:19 +0100

    .

--- notebooks/spelling.py
+++ notebooks/spelling.py
@@ -13,9 +13,10 @@ def correct_spelling(text):
     # lets Correct the misspelled words baby!________________________________________________
     #make a list to save the corrected word inside
     corrected_text = []
-
+    print (1)
     for word in words:
         if word in misspelled: #misspelled = spell.unknown(words)
+            print (2)
             corrected_text.append(spell.correction(word))
         else:
             corrected_text.append(word)
@@ -27,3 +28,31 @@ def correct_spelling(text):
     return corrected_text
 
 
+text = """The purpos of this blog post is just to test out projekt capability of extracting code from posts.
+
+```
+testtesttesttesttesttest
+test
+test
+test
+test
+```
+
+import requests
+import re
+from string_extraction_function import extract_text_between_strings
+from text_search_function import search_string_in_text
+from code_extraction_function import  extract_code_blocks
+from print_codeblocks_function import print_code_blocks
+
+# Zeichne die Ränder des Quadrats
+for i in range(seitenlänge):
+    for j in range(seitenlänge):
+        if i == 0 or i == seitenlänge - 1 or j == 0 or j == seitenlänge - 1:
+            print(zeichen, end=' ')
+        else:
+            if i==1:
+                print (" ", end=' ')
+            if i==2:"""
+corrected_text = correct_spelling(text)
+print(corrected_text)
--- notebooks/test6.py
+++ notebooks/test6.py
@@ -27,11 +27,11 @@ def index():
         # Call GitHub discussion function to get code blocks without markdown
         nomark_code_array = nomark_search_github_discussion_gql(github_username, repository_name, discussion_number, personal_token)
 
-        corrected_text_array = spelling_search_github_discussion_gql(github_username, repository_name, discussion_number, personal_token)
+        #corrected_text_array = spelling_search_github_discussion_gql(github_username, repository_name, discussion_number, personal_token)
 
 
         # Zeige das Ergebnis in der Webanwendung an
-        return render_template('result.html', code_blocks=code_blocks_array, nomark_code=nomark_code_array, corrected_text=corrected_text_array)          #nomark_code=nomark_code_array
+        return render_template('result.html', code_blocks=code_blocks_array, nomark_code=nomark_code_array, )          #nomark_code=nomark_code_array
     # Zeige das Eingabeformular an
     return render_template('index2.html')
 
